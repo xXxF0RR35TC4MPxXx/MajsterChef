c1e5ef21c3aff6f8e4624a5138c9152c330acae0
xXxF0RR35TC4MPxXx <matix2733@gmail.com>
2021-05-20 13:35:05 +0200

    Dodano: Wyszukanie przepisów po nazwie lub nazwie użytkownika

--- Pages/Przepisy/Index.cshtml.cs
+++ Pages/Przepisy/Index.cshtml.cs
@@ -25,10 +25,16 @@ namespace MajsterChef.Pages.Przepisy
             else return "Anonymous";
         }
         public IList<Przepis> Przepis { get;set; }
-
-        public async Task OnGetAsync()
-        {
-            Przepis = await _context.Przepis.OrderByDescending(u=>u.Data_publikacji).ToListAsync();
+        public async Task OnGetAsync(string searchString)
+        { 
+            IQueryable<Przepis> przepisyIQ = from s in _context.Przepis
+                                             select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                przepisyIQ = przepisyIQ.Where(s => s.Owner.Contains(searchString)
+                                       || s.Nazwa.Contains(searchString));
+            }
+            Przepis = await przepisyIQ.AsNoTracking().OrderByDescending(u=>u.Data_publikacji).ToListAsync();
         }
     }
 }
