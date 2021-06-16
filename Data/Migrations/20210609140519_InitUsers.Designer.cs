190be9379d22a183c1635dad2ae046bf1d6ec3a4
xXxF0RR35TC4MPxXx <matix2733@gmail.com>
2021-06-16 19:17:12 +0200

    Zmieniono: wygląd strony. To zrobił Łukasz, ale nie mógł wrzucić na repo przez jakieś problemy z githubem, więc zrobiłem to ja.

--- Data/Migrations/20210609140519_InitUsers.Designer.cs
+++ Data/Migrations/20210609140519_InitUsers.Designer.cs
@@ -4,14 +4,16 @@ using MajsterChef.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MajsterChef.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210609140519_InitUsers")]
+    partial class InitUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210609134245_Init1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MajsterChef.Migrations
+{
+    public partial class Init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Lista_skladnikow",
+                table: "Przepis",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Lista_skladnikow",
+                table: "Przepis",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000);
+        }
+    }
+}
